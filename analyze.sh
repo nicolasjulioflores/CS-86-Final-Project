7ebf15dacc40a9cfd3b88987f48fadb36f30b935
doh210 <davidoh210@gmail.com>
2018-06-03 21:55:50 -0400

    update analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -8,4 +8,20 @@
 #$ -l gpu
 
 source ~cs86/students/bin/PyRosetta.ScientificLinux-r53335.64Bit/SetPyRosettaEnvironment.sh
-python analyze.py 1le0_m1.pdb output1.pdb 
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_10000.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_1000.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_100.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_10.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_10000.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_1000.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_100.pdb
+python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_10.pdb
+
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_10000.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_1000.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_100.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_10.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_10000.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_1000.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_100.pdb
+python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_10.pdb
