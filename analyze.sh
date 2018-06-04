7e4acc8f067f71144b0f9bd2fbeee4e901e3d9c2
doh210 <davidoh210@gmail.com>
2018-06-03 22:11:20 -0400

    analysis for sims

--- analyze.sh
+++ analyze.sh
@@ -2,26 +2,53 @@
 #$ -j y
 #$ -cwd
 #$ -V
-#$ -l h_rt=12:00:00
+#$ -l h_rt=1:00:00
 #$ -l virtual_free=1
 #$ -l ironfs
 #$ -l gpu
 
 source ~cs86/students/bin/PyRosetta.ScientificLinux-r53335.64Bit/SetPyRosettaEnvironment.sh
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_10000.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_1000.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_100.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_centroid_10.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_10000.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_1000.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_100.pdb
-python analyze.py 4xdx.clean.pdb output_4xdx.clean_fullatom_10.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_centroid_10000.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_centroid_1000.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_centroid_100.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_centroid_10.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_fullatom_10000.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_fullatom_1000.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_fullatom_100.pdb
+python analyze.py 4yud.clean.pdb output_4yud.clean_fullatom_10.pdb
 
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_10000.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_1000.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_100.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_centroid_10.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_10000.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_1000.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_100.pdb
-python analyze.py 4ytv.clean.pdb output_4ytv.clean_fullatom_10.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_centroid_10000.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_centroid_1000.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_centroid_100.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_centroid_10.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_fullatom_10000.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_fullatom_1000.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_fullatom_100.pdb
+python analyze.py 5w0h.clean.pdb output_5w0h.clean_fullatom_10.pdb
+
+python analyze.py 6elm.clean.pdb output_6elm.clean_centroid_10000.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_centroid_1000.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_centroid_100.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_centroid_10.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_fullatom_10000.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_fullatom_1000.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_fullatom_100.pdb
+python analyze.py 6elm.clean.pdb output_6elm.clean_fullatom_10.pdb
+
+python analyze.py 1le0.clean.pdb output_1le0.clean_centroid_10000.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_centroid_1000.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_centroid_100.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_centroid_10.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_fullatom_10000.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_fullatom_1000.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_fullatom_100.pdb
+python analyze.py 1le0.clean.pdb output_1le0.clean_fullatom_10.pdb
+
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_centroid_10000.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_centroid_1000.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_centroid_100.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_centroid_10.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_fullatom_10000.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_fullatom_1000.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_fullatom_100.pdb
+python analyze.py 1ubq.clean.pdb output_1ubq.clean_fullatom_10.pdb
